7183e52c043c95415cc59f76786acc5c863942a5
chuppyy <takhanhthient3@GMAIL.COM>
2024-03-03 09:55:20 +0700

    kk6

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -64,6 +64,7 @@ export async function getStaticProps({ params }: { params: any }) {
     ).then((res) => res.json());
     return {
       props: { data: response.data },
+      revalidate: 60
     };
   } catch (error) {
     return {
