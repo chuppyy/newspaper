dc4a9c6412dba3afbbcc8c23177cd420eb9f531b
chuppyy <takhanhthient3@GMAIL.COM>
2024-03-04 22:24:14 +0700

    kk6

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -1,28 +1,36 @@
 // components/ConsentBanner.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ConsentBanner = () => {
-    const isAskCookies = localStorage.getItem("is-assk-cookie")
+    const [isAskCookies, setIsAskCookies] = useState(false);
+
+    useEffect(() => {
+        // Check localStorage only when running on client-side
+        if (typeof window !== 'undefined') {
+            const isAskCookiesStored = localStorage.getItem("is-ask-cookie");
+            setIsAskCookies(isAskCookiesStored === "true");
+        }
+    }, []);
+
     const handleConsent = () => {
-        // Update consent status when user agrees
         gtag('consent', 'update', {
             'ad_storage': 'granted'
-            // Update other consent parameters as needed
         });
 
-        // save to local storage
-        localStorage.setItem("is-assk-cookie", true)
+        // Save consent status to localStorage
+        setIsAskCookies(true);
+        localStorage.setItem("is-ask-cookie", "true");
     };
 
     return (
-        <>
+        <div>
             {!isAskCookies && (
                 <div className="consent-banner">
                     <p>We use cookies for analytics. Do you consent?</p>
                     <button onClick={handleConsent}>Yes</button>
                 </div>
             )}
-        </>
+        </div>
     );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,20 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=430115772"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '430115772');
+              `,
+          }}
+        />
       </Head>
 
       <Layout>
