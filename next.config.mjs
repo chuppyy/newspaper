489e553b62fd0a9f00a83972f8bcc6de35f130f3
chuppyy <takhanhthient3@GMAIL.COM>
2024-03-04 22:40:01 +0700

    kk6

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["apinewspaper.sportsandtravelonline.com","api.sportsandtravelonline.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 };
 
 export default nextConfig;
