3d1bc141cd472eb8288682fd733be499b08ef805
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-04-02 15:35:07 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -241,8 +241,7 @@ export default function Page(data: any) {
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8079422152195086"
       />
       <main>
-        <Script src="/qcscript.js" />
-
+      
         <div className="container-flu details">
           <script
             async
